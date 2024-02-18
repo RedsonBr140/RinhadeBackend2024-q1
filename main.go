4089a2f9989ec0108ab82eed0e3e8b786f65824d
Redson <redson@riseup.net>
2024-02-18 11:21:40 -0300

    Connect to the database

--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
 
 	"os"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	// urlExample := "postgres://user:pass@localhost:5432/db_name"
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Connecting to the database: %v\n", err)
+	}
 
+	defer conn.Close(context.Background())
+
+	// Create router
+	e := echo.New()
+	// Register routes
+	e.GET("/", HelloHandler)
+	// Start router
 	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
+}
+
+func HelloHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
